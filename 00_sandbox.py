9fa00d0d209f95c612ad1bbd98565aaab50c4d9d
konelliusbriggs <78515960+konelliusbriggs@users.noreply.github.com>
2021-03-25 14:55:22 +1300

    Add files via upload

---
+++ 00_sandbox.py
@@ -0,0 +1,8 @@
+# ask users for a number
+get_number = int(input("choose a number?"))
+
+# multiply the number by 5
+times_five = get_number * 5
+
+#output the result
+print("{} times five is equal to {}". format(get_number, times_five))
---
+++ token_genarator_2_broken.py
@@ -0,0 +1,5 @@
+  import random   
+
+  for item in range(0,20):
+    number =  random.radint(0, 4)
+    print(number, end=\t")
